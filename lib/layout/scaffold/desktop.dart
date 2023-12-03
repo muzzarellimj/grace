92c0f1851f087c9b19459435290f801086b7ac90
Michael Muzzarelli <muzzarellimj@outlook.com>
2023-12-03 00:56:53 -0500

    feat: add brand-fill asset, revise desktop scaffold

--- lib/layout/scaffold/desktop.dart
+++ lib/layout/scaffold/desktop.dart
@@ -6,9 +6,11 @@ class GraceDesktopScaffold extends StatelessWidget {
   final ValueChanged<int> onDestinationSelected;
 
   static const Color _backgroundColor = Color.fromRGBO(245, 243, 240, 1.0);
-  static const double _iconSize = 28.0;
+  static const double _iconSize = 30.0;
+  static const double _imageSize = 36.0;
   static const Color _iconColor = Color.fromRGBO(4, 64, 20, 1.0);
   static const Color _indicatorColor = Color.fromRGBO(4, 64, 20, 0.15);
+  static const double _yPadding = 28.0;
 
   const GraceDesktopScaffold(
       {super.key,
@@ -26,9 +28,12 @@ class GraceDesktopScaffold extends StatelessWidget {
           onDestinationSelected: onDestinationSelected,
           destinations: const [
             NavigationRailDestination(
-                label: Text('Home'), icon: Icon(Icons.home)),
+                label: Text('Home'),
+                icon: Icon(Icons.home),
+                padding:
+                    EdgeInsets.only(top: _yPadding, bottom: _yPadding / 2)),
             NavigationRailDestination(
-                label: Text('Collections'), icon: Icon(Icons.library_books))
+                label: Text('Collections'), icon: Icon(Icons.library_books)),
           ],
           backgroundColor: _backgroundColor,
           indicatorColor: _indicatorColor,
@@ -36,6 +41,11 @@ class GraceDesktopScaffold extends StatelessWidget {
               const IconThemeData(size: _iconSize, color: _iconColor),
           unselectedIconTheme:
               const IconThemeData(size: _iconSize, color: _iconColor),
+          leading: Container(
+              alignment: Alignment.topCenter,
+              padding: const EdgeInsets.only(top: _yPadding, bottom: _yPadding),
+              child: Image.asset('assets/images/brand-fill.png',
+                  height: _imageSize)),
         ),
         Expanded(child: body)
       ],
