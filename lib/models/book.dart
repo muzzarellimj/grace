ab85fd8632e2e75bd670242cdd3c22019a9f67d8
Michael Muzzarelli <muzzarellimj@outlook.com>
2023-12-05 09:59:42 -0500

    feat: revise book model, expand data captured and allow nullish data

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -1,17 +1,40 @@
 class Book {
   String title;
-  String author;
-  String publicationDate;
-  String isbn;
+  String? description;
+  String? excerpt;
+  List? authors;
+  List? publishers;
+  List? contributors;
+  int? pageCount;
+  String? publicationDate;
+  String? isbn10;
+  String? isbn13;
 
-  Book({ required this.title, required this.author, required this.publicationDate, required this.isbn });
+  Book({
+    required this.title,
+    required this.description,
+    required this.excerpt,
+    this.authors,
+    required this.publishers,
+    this.contributors,
+    required this.pageCount,
+    required this.publicationDate,
+    required this.isbn10,
+    required this.isbn13,
+  });
 
   factory Book.fromJson(Map<String, dynamic> data) {
     return Book(
-      title: data['title'], 
-      author: data['author'], 
+      title: data['title'],
+      description: data['description'],
+      excerpt: data['excerpt'],
+      authors: data['authors'],
+      publishers: data['publishers'],
+      contributors: data['contributors'],
+      pageCount: data['pageCount'],
       publicationDate: data['publicationDate'],
-      isbn: data['isbn']
+      isbn10: data['isbn10'],
+      isbn13: data['isbn13'],
     );
   }
 }
