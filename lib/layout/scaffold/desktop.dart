fefdd7009e8d3ddc9c362125854bc4eed2e0ffbb
Michael Muzzarelli <muzzarellimj@outlook.com>
2023-12-05 18:45:40 -0500

    refactor: temporarily remove collections screen, disable navigation

--- lib/layout/scaffold/desktop.dart
+++ lib/layout/scaffold/desktop.dart
@@ -12,43 +12,56 @@ class GraceDesktopScaffold extends StatelessWidget {
   static const Color _indicatorColor = Color.fromRGBO(4, 64, 20, 0.15);
   static const double _yPadding = 28.0;
 
-  const GraceDesktopScaffold(
-      {super.key,
-      required this.body,
-      required this.selectedIndex,
-      required this.onDestinationSelected});
+  const GraceDesktopScaffold({
+    super.key,
+    required this.body,
+    required this.selectedIndex,
+    required this.onDestinationSelected,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Row(
-      children: [
-        NavigationRail(
-          selectedIndex: selectedIndex,
-          onDestinationSelected: onDestinationSelected,
-          destinations: const [
-            NavigationRailDestination(
+      body: Row(
+        children: [
+          NavigationRail(
+            selectedIndex: selectedIndex,
+            onDestinationSelected: onDestinationSelected,
+            destinations: const [
+              NavigationRailDestination(
                 label: Text('Home'),
                 icon: Icon(Icons.home),
-                padding:
-                    EdgeInsets.only(top: _yPadding, bottom: _yPadding / 2)),
-            NavigationRailDestination(
-                label: Text('Collections'), icon: Icon(Icons.library_books)),
-          ],
-          backgroundColor: _backgroundColor,
-          indicatorColor: _indicatorColor,
-          selectedIconTheme:
-              const IconThemeData(size: _iconSize, color: _iconColor),
-          unselectedIconTheme:
-              const IconThemeData(size: _iconSize, color: _iconColor),
-          leading: Container(
+                padding: EdgeInsets.only(top: _yPadding, bottom: _yPadding / 2),
+              ),
+              NavigationRailDestination(
+                label: Text('Collection'),
+                icon: Icon(Icons.library_books),
+                padding: EdgeInsets.only(top: _yPadding, bottom: _yPadding / 2),
+                disabled: true,
+              ),
+            ],
+            backgroundColor: _backgroundColor,
+            indicatorColor: _indicatorColor,
+            selectedIconTheme: const IconThemeData(
+              size: _iconSize,
+              color: _iconColor,
+            ),
+            unselectedIconTheme: const IconThemeData(
+              size: _iconSize,
+              color: _iconColor,
+            ),
+            leading: Container(
               alignment: Alignment.topCenter,
               padding: const EdgeInsets.only(top: _yPadding, bottom: _yPadding),
-              child: Image.asset('assets/images/brand-fill.png',
-                  height: _imageSize)),
-        ),
-        Expanded(child: body)
-      ],
-    ));
+              child: Image.asset(
+                'assets/images/brand-fill.png',
+                height: _imageSize,
+              ),
+            ),
+          ),
+          Expanded(child: body)
+        ],
+      ),
+    );
   }
 }
--- lib/layout/scaffold/mobile.dart
+++ lib/layout/scaffold/mobile.dart
@@ -10,11 +10,12 @@ class GraceMobileScaffold extends StatelessWidget {
   static const Color _iconColor = Color.fromRGBO(4, 64, 20, 1.0);
   static const Color _indicatorColor = Color.fromRGBO(4, 64, 20, 0.15);
 
-  const GraceMobileScaffold(
-      {super.key,
-      required this.body,
-      required this.selectedIndex,
-      required this.onDestinationSelected});
+  const GraceMobileScaffold({
+    super.key,
+    required this.body,
+    required this.selectedIndex,
+    required this.onDestinationSelected,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -25,12 +26,22 @@ class GraceMobileScaffold extends StatelessWidget {
         onDestinationSelected: onDestinationSelected,
         destinations: const [
           NavigationDestination(
-              label: 'Home',
-              icon: Icon(Icons.home, color: _iconColor, size: _iconSize)),
+            label: 'Home',
+            icon: Icon(
+              Icons.home,
+              color: _iconColor,
+              size: _iconSize,
+            ),
+          ),
           NavigationDestination(
-              label: 'Collections',
-              icon:
-                  Icon(Icons.library_books, color: _iconColor, size: _iconSize))
+            label: 'Collections',
+            icon: Icon(
+              Icons.library_books,
+              color: Colors.grey,
+              size: _iconSize,
+            ),
+            enabled: false,
+          )
         ],
         backgroundColor: _backgroundColor,
         indicatorColor: _indicatorColor,
