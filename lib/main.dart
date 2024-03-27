71ef4619bc1ca93b3eee3ba30303fe3e279fc516
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-26 20:56:21 -0400

    feat: add flutter_dotenv dependency and .env.sample, load on app init

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:grace/firebase_options.dart';
 import 'package:grace/router.dart';
 
 void main() async {
+  await dotenv.load();
+
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
