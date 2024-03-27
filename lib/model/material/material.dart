aa1daf1ededcfcb88597a4b7fa7666e4644070ad
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-27 19:04:49 -0400

    feat: add matFactory and setFactory to material enum, create search service function

--- lib/model/material/material.dart
+++ lib/model/material/material.dart
@@ -1,17 +1,34 @@
 import 'package:grace/model/material/book/book.dart';
+import 'package:grace/model/material/book/search_result.dart';
 import 'package:grace/model/material/game/game.dart';
+import 'package:grace/model/material/game/search_result.dart';
 import 'package:grace/model/material/movie/movie.dart';
+import 'package:grace/model/material/movie/search_result.dart';
 
 enum Material {
-  book(endpoint: '/api/book', parser: Book.fromJson),
-  game(endpoint: '/api/game', parser: Game.fromJson),
-  movie(endpoint: '/api/movie', parser: Movie.fromJson);
+  book(
+    endpoint: '/api/book',
+    matFactory: Book.fromJson,
+    setFactory: BookSearchResult.fromJson,
+  ),
+  game(
+    endpoint: '/api/game',
+    matFactory: Game.fromJson,
+    setFactory: GameSearchResult.fromJson,
+  ),
+  movie(
+    endpoint: '/api/movie',
+    matFactory: Movie.fromJson,
+    setFactory: MovieSearchResult.fromJson,
+  );
 
   final String endpoint;
-  final Function parser;
+  final Function matFactory;
+  final Function setFactory;
 
   const Material({
     required this.endpoint,
-    required this.parser,
+    required this.matFactory,
+    required this.setFactory,
   });
 }
--- lib/service/material_service.dart
+++ lib/service/material_service.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:grace/api/material_api.dart';
 import 'package:grace/model/material/material.dart';
 import 'package:grace/model/response/get_material.dart';
+import 'package:grace/model/response/get_material_set.dart';
 import 'package:grace/model/response/response_status.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,36 +13,62 @@ class MaterialService<M> {
 
   MaterialService({required this.material});
 
+  final String defaultErrorMessage = 'Oops! We hit a snag. Please try again.';
+
   Future<GetMaterialResponse<M>> fetch(int id) async {
-    http.Response response = await MaterialApi.fetch(this.material, id);
+    http.Response response = await MaterialApi.fetch(material, id);
 
     switch (response.statusCode) {
       case HttpStatus.ok:
         return GetMaterialResponse<M>(
           status: ResponseStatus.success,
           message: null,
-          material: material.parser(jsonDecode(response.body)['data']),
+          material: material.matFactory(jsonDecode(response.body)['data']),
         );
 
-      default:
-        return processResponse(response.statusCode);
-    }
-  }
-
-  GetMaterialResponse<M> processResponse(int status) {
-    switch (status) {
       case HttpStatus.noContent || HttpStatus.badRequest:
         return GetMaterialResponse<M>(
           status: ResponseStatus.warning,
-          message: 'A matching material could not be found. Please try again.',
+          message: 'A material could not be found. Please try again.',
           material: null,
         );
 
       default:
         return GetMaterialResponse<M>(
           status: ResponseStatus.failure,
-          message: 'Oops! We hit a snag. Please try again later.',
+          message: defaultErrorMessage,
           material: null,
+        );
+    }
+  }
+
+  Future<GetMaterialSetResponse<M>> search(String query) async {
+    http.Response response = await MaterialApi.search(material, query);
+
+    switch (response.statusCode) {
+      case HttpStatus.ok:
+        List<M> results = (jsonDecode(response.body)['data'] as List)
+            .map<M>((result) => material.setFactory(result))
+            .toList();
+
+        return GetMaterialSetResponse<M>(
+          status: ResponseStatus.success,
+          message: null,
+          materials: results,
+        );
+
+      case HttpStatus.noContent || HttpStatus.badRequest:
+        return GetMaterialSetResponse<M>(
+          status: ResponseStatus.warning,
+          message: 'A material could not be found. Please try again.',
+          materials: List.empty(),
+        );
+
+      default:
+        return GetMaterialSetResponse<M>(
+          status: ResponseStatus.failure,
+          message: defaultErrorMessage,
+          materials: List.empty(),
         );
     }
   }
