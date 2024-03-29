c8a75e4aef42a22b63771f3b41a519568edb1650
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-28 23:10:57 -0400

    refactor: revise HomeScreen with FutureBuilder, MaterialService, Collection widgets

--- lib/router.dart
+++ lib/router.dart
@@ -1,35 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:grace/layout/scaffold/base.dart';
-import 'package:grace/screens/home.dart';
+import 'package:grace/screen/home.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
 final _homeNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'home');
 
 class GraceRouter {
-  static GoRouter router =
-      GoRouter(navigatorKey: _rootNavigatorKey, initialLocation: '/', routes: [
-    StatefulShellRoute.indexedStack(
-      builder: (
-        BuildContext context,
-        GoRouterState state,
-        StatefulNavigationShell navigationShell,
-      ) {
-        return GraceBaseScaffold(
-          navigationShell: navigationShell,
-        );
-      },
-      branches: [
-        StatefulShellBranch(
-          navigatorKey: _homeNavigatorKey,
-          routes: [
-            GoRoute(
+  static GoRouter router = GoRouter(
+    navigatorKey: _rootNavigatorKey,
+    initialLocation: '/',
+    routes: [
+      StatefulShellRoute.indexedStack(
+        builder: (
+          BuildContext context,
+          GoRouterState state,
+          StatefulNavigationShell navigationShell,
+        ) {
+          return GraceBaseScaffold(
+            navigationShell: navigationShell,
+          );
+        },
+        branches: [
+          StatefulShellBranch(
+            navigatorKey: _homeNavigatorKey,
+            routes: [
+              GoRoute(
                 path: '/',
                 pageBuilder: (BuildContext context, GoRouterState state) =>
-                    const NoTransitionPage(child: HomeScreen()))
-          ],
-        ),
-      ],
-    )
-  ]);
+                    const NoTransitionPage(
+                  child: HomeScreen(),
+                ),
+              )
+            ],
+          ),
+        ],
+      )
+    ],
+  );
 }
