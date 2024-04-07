2b3157fb210c05e984423946051a21a4eb567745
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-04-07 18:51:51 -0400

    feat: create theme measurement sizing function

--- lib/theme/measurement.dart
+++ lib/theme/measurement.dart
@@ -1,5 +1,12 @@
 class Measurement {
   static const double defaultSpacing = 8.0;
+  static const double defaultSizing = 12.0;
+
+  static double getSizing(double factor) {
+    double calculatedSizing = defaultSizing * factor;
+
+    return calculatedSizing > 0 ? calculatedSizing : defaultSizing;
+  }
 
   static double getSpacing(double factor) {
     double calculatedSpacing = defaultSpacing * factor;
