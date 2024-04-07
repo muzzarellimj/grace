2ef6183b26d7b8a0996eb27fdf23049ddebfb859
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-04-07 19:31:02 -0400

    refactor: revise router to use home, collections, settings screens with responsive layout

--- lib/router.dart
+++ lib/router.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:grace/layout/scaffold/base.dart';
+import 'package:grace/screen/collections.dart';
 import 'package:grace/screen/home.dart';
-
-final _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
-final _homeNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'home');
+import 'package:grace/screen/settings.dart';
+import 'package:grace/widget/layout/responsive.dart';
 
 class GraceRouter {
+  static final rootNavigationKey =
+      GlobalKey<NavigatorState>(debugLabel: 'root');
+  static final homeNavigationKey =
+      GlobalKey<NavigatorState>(debugLabel: 'home');
+  static final collectionsNavigationKey =
+      GlobalKey<NavigatorState>(debugLabel: 'collections');
+  static final settingsNavigationKey =
+      GlobalKey<NavigatorState>(debugLabel: 'settings');
+
   static GoRouter router = GoRouter(
-    navigatorKey: _rootNavigatorKey,
+    navigatorKey: rootNavigationKey,
     initialLocation: '/',
     routes: [
       StatefulShellRoute.indexedStack(
@@ -17,20 +25,55 @@ class GraceRouter {
           GoRouterState state,
           StatefulNavigationShell navigationShell,
         ) {
-          return GraceBaseScaffold(
+          return ResponsiveLayout(
             navigationShell: navigationShell,
           );
         },
         branches: [
           StatefulShellBranch(
-            navigatorKey: _homeNavigatorKey,
+            navigatorKey: homeNavigationKey,
             routes: [
               GoRoute(
-                name: '/',
+                name: 'home',
                 path: '/',
-                pageBuilder: (BuildContext context, GoRouterState state) =>
+                pageBuilder: (
+                  BuildContext context,
+                  GoRouterState state,
+                ) =>
                     const NoTransitionPage(
                   child: HomeScreen(),
+                ),
+              )
+            ],
+          ),
+          StatefulShellBranch(
+            navigatorKey: collectionsNavigationKey,
+            routes: [
+              GoRoute(
+                name: 'collections',
+                path: '/collections',
+                pageBuilder: (
+                  BuildContext context,
+                  GoRouterState state,
+                ) =>
+                    const NoTransitionPage(
+                  child: CollectionsScreen(),
+                ),
+              )
+            ],
+          ),
+          StatefulShellBranch(
+            navigatorKey: settingsNavigationKey,
+            routes: [
+              GoRoute(
+                name: 'settings',
+                path: '/settings',
+                pageBuilder: (
+                  BuildContext context,
+                  GoRouterState state,
+                ) =>
+                    const NoTransitionPage(
+                  child: SettingsScreen(),
                 ),
               )
             ],
